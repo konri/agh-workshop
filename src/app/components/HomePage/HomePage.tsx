0979e75aa6c8f8c5775a444b8bc6a53e262df1f1
Konrad Hopek <konradh@qualtrics.com>
2021-10-13 16:26:47 +0200

    add styled-components

--- src/app/components/HomePage/HomePage.tsx
+++ src/app/components/HomePage/HomePage.tsx
@@ -9,17 +9,6 @@ export const HomePage = () => {
     <header className="App-header">
       <img src={logo} className="App-logo" alt="logo" />
       <Counter />
-      <p>
-        Edit <code>src/App.tsx</code> and save to reload.
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
     </header>
   </div>)
 }
