a605771102fbb9790088b6705388f62f5875e180
Konrad Hopek <konradh@qualtrics.com>
2021-10-14 12:28:56 +0200

    Add header for application

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,34 @@ import {
 } from "react-router-dom";
 
 import { HomePage } from './app/components/HomePage/HomePage';
+import AghLogo from './agh-logo.png';
+import QualtricsLogo from './Qualtrics-logo.svg';
+import styled from 'styled-components';
+
+const Header = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  background-color: white;
+  height: 100px;
+  width: 100%;
+  padding: 10px 20px;
+`
+
+const AppName = styled.div`
+  font-size: 32px;
+  color: #282c34;
+`
 
 function App() {
   return (
+    <div>
+      <Header>
+        <img src={AghLogo} className="logo" alt="AghLogo" />
+        <AppName>Retrospective application</AppName>
+        <img src={QualtricsLogo} className="logo" alt="QualtricsLogo" />
+      </Header>
+
     <Router>
       <div>
         <Switch>
@@ -18,6 +43,7 @@ function App() {
         </Switch>
       </div>
     </Router>
+    </div>
   );
 }
 
--- src/app/components/HomePage/HomePage.tsx
+++ src/app/components/HomePage/HomePage.tsx
@@ -3,10 +3,13 @@ import logo from '../../../logo.svg';
 import './HomePage.css';
 import { Counter } from '../Counter/Counter';
 
+
 export const HomePage = () => {
   return (
     <div className="App">
+
     <header className="App-header">
+
       <img src={logo} className="App-logo" alt="logo" />
       <Counter />
     </header>
